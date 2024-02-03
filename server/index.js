cb1327af6b5f2888cbe239141fd2be90d90d1d69
arkar phyo <115158583+ArkarPhyo8@users.noreply.github.com>
2024-02-03 23:39:24 +0630

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://airbnb-app-frontend.vercel.app",
   })
 );
 app.use("/uploads", express.static(__dirname + "/uploads"));
