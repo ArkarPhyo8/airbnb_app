cd039b91932913d9d77af6885210f1d9f6c89683
arkar phyo <115158583+ArkarPhyo8@users.noreply.github.com>
2024-02-03 23:14:30 +0630

    Update index.js

--- server/index.js
+++ server/index.js
@@ -108,19 +108,19 @@ app.post("/upload-by-link", async (req, res) => {
   res.json(newName);
 });
 
-const photoMiddleware = multer({ dest: "uploads" });
-app.post("/upload", photoMiddleware.array("photos", 100), (req, res) => {
-  const uploadedFiles = [];
-  for (let i = 0; i < req.files.length; i++) {
-    const { path, originalname } = req.files[i];
-    const parts = originalname.split(".");
-    const ext = parts[parts.length - 1];
-    newPath = path + "." + ext;
-    fs.renameSync(path, newPath);
-    uploadedFiles.push(newPath.replace("uploads/", ""));
-  }
-  res.json(uploadedFiles);
-});
+// const photoMiddleware = multer({ dest: "uploads" });
+// app.post("/upload", photoMiddleware.array("photos", 100), (req, res) => {
+//   const uploadedFiles = [];
+//   for (let i = 0; i < req.files.length; i++) {
+//     const { path, originalname } = req.files[i];
+//     const parts = originalname.split(".");
+//     const ext = parts[parts.length - 1];
+//     newPath = path + "." + ext;
+//     fs.renameSync(path, newPath);
+//     uploadedFiles.push(newPath.replace("uploads/", ""));
+//   }
+//   res.json(uploadedFiles);
+// });
 
 // app.post("/places", (req, res) => {
 //   mongoose.connect(process.env.MONGO_URL);
