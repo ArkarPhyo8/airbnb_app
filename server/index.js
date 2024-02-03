6cef48669daea4f7248be52d0769506a1b679e27
arkar phyo <115158583+ArkarPhyo8@users.noreply.github.com>
2024-02-03 23:10:51 +0630

    Update index.js

--- server/index.js
+++ server/index.js
@@ -39,21 +39,21 @@ app.get("/", (req, res) => {
   res.json("Welcome to airbnb app");
 });
 
-// app.post("/register", async (req, res) => {
-//   mongoose.connect(process.env.MONGO_URL);
-//   const { name, email, password } = req.body;
+app.post("/register", async (req, res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const { name, email, password } = req.body;
 
-//   try {
-//     const userDoc = await User.create({
-//       name,
-//       email,
-//       password: bcrypt.hashSync(password, bcryptSalt),
-//     });
-//     res.json(userDoc);
-//   } catch (e) {
-//     res.status(422).json(e);
-//   }
-// });
+  try {
+    const userDoc = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(422).json(e);
+  }
+});
 
 // app.post("/login", async (req, res) => {
 //   mongoose.connect(process.env.MONGO_URL);
