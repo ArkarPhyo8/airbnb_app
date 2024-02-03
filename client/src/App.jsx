04b373b6f584dfb50dc45016e14f8a4671cf906a
arkar phyo <115158583+ArkarPhyo8@users.noreply.github.com>
2024-02-03 23:38:33 +0630

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ import PlacePage from "./pages/PlacePage";
 import BookingsPage from "./pages/BookingsPage";
 import BookingPage from "./pages/BookingPage";
 
-axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.baseURL = "https://airbnb-app-mu.vercel.app";
 axios.defaults.withCredentials = true;
 
 function App() {
